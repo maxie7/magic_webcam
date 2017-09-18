58e0cf3e38096daeb402f38a4be291c2e8f3f300
Kos Max <maxie7@protonmail.com>
2017-09-18 23:31:51 +0200

    Add takePhoto()

--- scripts.js
+++ scripts.js
@@ -27,4 +27,20 @@ function paintToCanvas() {
     }, 16);
 }
 
+function takePhoto() {
+    // play the sound
+    snap.currentTime = 0;
+    snap.play();
+
+    //take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.textContent = 'Download Image';
+    strip.insertBefore(link, strip.firstChild);
+}
+
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
